a5ff675a39bdd0fcedef3b22224ac1da1bf7274f
TEH JIA JIE <tehjiajie@gmail.com>
2025-11-10 10:37:16 +0800

    hehehehehe gg

---
+++ Lab1/L1Q2/src/l1q2/MyProfile.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l1q2;
+
+/**
+ *
+ * @author tehjiajie
+ */
+public class MyProfile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("=============My Profile=============");
+        System.out.println("Name: Teh Jia Jie");
+        System.out.println("Matric number: 25006754");
+        System.out.println("Faculty: FSKTM");
+        System.out.println("Course: Artificial Intelligence");
+        System.out.println("Address: UM ");
+        System.out.println("Contact: 017-5566137");
+        System.out.println("====================================");
+    }
+    
+}
