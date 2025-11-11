bf5d0c30197c4aacfa433ed27fea4fb27a037808
TEH JIA JIE <tehjiajie@gmail.com>
2025-11-11 13:39:25 +0800

    Lab

---
+++ Lab_5/L5Q1/src/l5q1/L5Q1.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l5q1;
+
+/**
+ *
+ * @author tehjiajie
+ * 1. Write a program to randomly generate N student scores (0-100). The program will 
+prompt the user to enter N students and store the score in an array. Then, the program 
+will display a list of score, the highest score, the lowest score and the average score. 
+ */
+import java.util.*;
+public class L5Q1 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of student: ");
+        int N = sc.nextInt();
+        int[] score = new int[N];
+        int max = 0;
+        int min = 101;
+        System.out.println("List of score: ");
+        for(int i=0; i<N; i++){
+            score[i] = rand.nextInt(101);
+            System.out.println("Student "+(i+1)+": "+score[i]);
+            
+        }
+        Arrays.sort(score);
+        System.out.println("The highest score is: "+score[N-1]);
+        System.out.println("The lowest score is: "+score[0]);
+    }
+    
+}
---
+++ Lab_5/L5Q2/src/l5q2/L5Q2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package l5q2;
+
+/**
+ *
+ * @author tehjiajie
+ * 2. Write a program that generates 10 non-duplicate random integers within the range 
+from 0 to 20. 
+ */
+import java.util.*;
+public class L5Q2 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] integer = new int[10];
+        boolean duplicate;
+        for(int i=0; i<10; i++){
+            integer[i] = rand.nextInt(21);
+            for(int j=0; j<i; j++){
+                duplicate = integer[i]==integer[j];
+                while(duplicate){
+                    integer[i] = rand.nextInt(21);
+                    duplicate = integer[i] == integer[j];
+                }
+            }
+            System.out.println(integer[i]);
+            }
+    }
+    
+}
